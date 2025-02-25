9e62c47bc3d7b5848206901c0c512174254f0c7d
muhammadhnhtechsolutions <muhammad@hnhtechsolutions.com>
2025-02-25 20:02:56 +0500

    'change_based_url'

--- src/utils/basedUrl.jsx
+++ src/utils/basedUrl.jsx
@@ -1,3 +1,4 @@
 // export const basedUrl = "https://192.168.114.19:8000/";
-export const basedUrl = "http://139.99.237.187:7009/";
+// export const basedUrl = "http://139.99.237.187:7009/";
+export const basedUrl = "https://clay.devssh.xyz/";
 
